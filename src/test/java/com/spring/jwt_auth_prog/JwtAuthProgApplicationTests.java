f0c7f9487bee4d0e774e3818434d04acfee27dbd
Kumar Gaurav <k09gaurav@gmail.com>
2025-09-22 16:45:38 +0530

    init

---
+++ src/test/java/com/spring/jwt_auth_prog/JwtAuthProgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jwt_auth_prog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthProgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
